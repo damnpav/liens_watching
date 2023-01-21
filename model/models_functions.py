e47582af172e515a7dbce15a7399eb1b3a2a7390
damnpav <hcanilvap@gmail.com>
2023-01-21 13:50:46 +0300

    prepare regression model functions

--- model/models_functions.py
+++ model/models_functions.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 data_path = '../lien_df1_18_01_23.csv'
@@ -19,6 +23,32 @@ def encode_features(selected_df, features_list):
     return label_encoded_df
 
 
+def make_regression(data_df, target_col):
+
+    # splitting data in train and test datasets, also in target and features (X,Y)
+    train_df, test_df = train_test_split(data_df, test_size=0.2, shuffle=True)
+    train_X = train_df[list(set(train_df.columns) - set([target_col]))]
+    train_Y = train_df[target_col]
+    test_X = test_df[list(set(test_df.columns) - set([target_col]))]
+    test_Y = test_df[target_col]
+
+    # objects of model and scaler
+    regression_model = LinearRegression()
+    stdsc = StandardScaler()
+
+    # scale data
+    X_train_std = stdsc.fit_transform(train_X)
+    Y_train_std = stdsc.fit_transform(np.array(train_Y).reshape(-1, 1))
+    X_test_std = stdsc.fit_transform(test_X)
+    Y_test_std = stdsc.fit_transform(np.array(test_Y).reshape(-1, 1))
+
+
+    # fit model
+    model = regression_model.fit(X_train_std, Y_train_std)
+
+    # todo c этим надо еще поработать - навестить функцию тестирования, показа результатов и тд
+
+    return model
 
 
 
