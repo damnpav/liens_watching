413cd75554707a7d805500804cf786b05883e388
damnpav <hcanilvap@gmail.com>
2023-11-07 21:53:52 +0300

    db logging

--- server_version/playwright_parser_function.py
+++ server_version/playwright_parser_function.py
@@ -26,7 +26,7 @@ def click_and_safe(section_link, links, db_conn, db_cur):
             browser_type = p.chromium
             browser = browser_type.launch(headless=False)
             page = browser.new_page()
-            print('Open main tab')
+            logging('Open main tab', db_conn, db_cur)
             page.goto(section_link)
             time.sleep(3)
 
@@ -34,63 +34,59 @@ def click_and_safe(section_link, links, db_conn, db_cur):
             flag = True
             k = 0
             while flag and k < 2:
-                print(f'Show more offers {k}')
+                logging(f'Show more offers {k}', db_conn, db_cur)
                 try:
                     page.get_by_role("button", name="Показать ещё").click()
                 except Exception as e:
-                    print('All offers are shown')
-                    print(f'Exception: {e}')
+                    logging('All offers are shown', db_conn, db_cur)
+                    logging(f'Exception: {e}', db_conn, db_cur)
                     flag = False
                 k += 1
                 time.sleep(1)
 
-            print(f'Open first offer')
+            logging(f'Open first offer', db_conn, db_cur)
             with page.expect_popup() as page1_info:
                 page.locator(".asset-card__photos > a").first.click()
             page1 = page1_info.value
             links.append(page1.url)
             page1.close()
 
-            print(f'Open another offers')
+            logging(f'Open another offers', db_conn, db_cur)
             flag = True
             k = 1
             while flag:
-                print(dt.now().strftime('%H:%M:%S'))
                 time.sleep(1)
                 try:
-                    print(f'Clicking anoter offer: {k}')
+                    logging(f'Clicking anoter offer: {k}', db_conn, db_cur)
                     with page.expect_popup() as page1_info:
                         page.locator(f"div:nth-child({k}) > .asset-card__content-wrapper > .asset-card__photos > a").click()
                     page1 = page1_info.value
                     current_link = page1.url
 
-
-
-                    if 'chrome-error' not in current_link:
-                        links.append(current_link)
-                        page1.close()
-                    else:
-                        print(f'chrome error here, taking screenshot')
+                    if 'chrome-error' in current_link:
+                        logging(f'chrome error here, taking screenshot', db_conn, db_cur)
                         page1.screenshot(path=rf"screens\screenshot_error_{k}.png")
                         page.screenshot(path=rf'screens\screenshot_error_main_tab_{k}.png')
-                        print(f'lets wait and try again')
+                        logging(f'lets wait and try again', db_conn, db_cur)
                         page1.close()
                         time.sleep(10)
-                        print(f'Clicking another offer: {k}')
+                        logging(f'Clicking another offer: {k}', db_conn, db_cur)
                         with page.expect_popup() as page1_info:
                             page.locator(
                                 f"div:nth-child({k}) > .asset-card__content-wrapper > .asset-card__photos > a").click()
                         page1 = page1_info.value
                         current_link = page1.url
-                        links.append(current_link)
-                        save_link(current_link, db_cur, db_conn)
+
+                    page1.close()
+                    links.append(current_link)
+                    save_link(current_link, db_cur, db_conn)
                     k += 1
                 except Exception as e:
-                    print('All links are clicked')
-                    print(f'Exception: {e}')
+                    logging('All links are clicked', db_conn, db_cur)
+                    logging(f'Exception: {e}', db_conn, db_cur)
                     flag = False
     except Exception as e:
-        print(f'Global exception: {e}')
+        logging(f'Global exception: {e}', db_conn, db_cur)
     return links
 
 
@@ -103,31 +99,42 @@ def save_link(link_str, cur, conn):
     :return:
     """
     try:
-        cur.execute("INSERT INTO links VALUES ('{link_str}')")
+        cur.execute(f"INSERT INTO links VALUES ('{link_str}')")
         conn.commit()
         print(f'Link {link_str} appended to db')
     except Exception as e:
-        print(f'Exception at database writing: {str(e)}')
+        print(f'Exception at database writing (save_link): {str(e)}')
     return None
 
 
-print('Connecting to db')
-conn = sqlite3.connect(path_to_db)
-cur = conn.cursor()
+def logging(log_str, cur, conn):
+    """
+    Log to db
+    :param log_str:
+    :return:
+    """
+    try:
+        cur.execute(f"INSERT INTO logging VALUES ('{dt.now().strftime('%Y-%m-%d %H-%M-%S')}', '{log_str}')")
+        conn.commit()
+    except Exception as e:
+        print(f'Exception at database writing (logging): {str(e)}')
+    return None
 
 
-print(f'start')
-links_list = click_and_safe(section_link, links, conn, cur)
-print(f'{len(links_list)} links are saved')
+def main():
+    print('Connecting to db')
+    conn = sqlite3.connect(path_to_db)
+    cur = conn.cursor()
+    logging('start', cur, conn)
+    links_list = click_and_safe(section_link, links, conn, cur)
+    logging(f'{len(links_list)} links are saved', cur, conn)
+    conn.close()
 
-# print(f'save links to db')
-# conn = sqlite3.connect(path_to_db)
-# result_df.to_sql('links', conn, if_exists='append')
 
-conn.close()
+try:
+    main()
+except Exception as e:
+    print(f'Exception in main: {str(e)}')
 
-#result_df.to_excel('portal_da_links_lands_10_01_23_2.xlsx', index=False)
-
-# TODO - done, now need in parser per link
 
 
